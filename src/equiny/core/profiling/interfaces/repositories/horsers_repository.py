4596a29bdbb6b8dc99fb736bf08a4bbc269def44
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-11 22:25:59 -0300

    📑 interface: update horses repository interface

--- src/equiny/core/profiling/interfaces/repositories/horsers_repository.py
+++ src/equiny/core/profiling/interfaces/repositories/horsers_repository.py
@@ -1,10 +1,8 @@
-from abc import ABC, abstractmethod
+from typing import Protocol
 from equiny.core.profiling.domain.entities.horse import Horse
 
 
-class HorsesRepository(ABC):
-    @abstractmethod
+class HorsesRepository(Protocol):
     def add(self, horse: Horse) -> None: ...
 
-    @abstractmethod
     def find_by_id(self, horse_id: str) -> Horse | None: ...
