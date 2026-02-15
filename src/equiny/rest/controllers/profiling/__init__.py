bc9cd7ea6e78c3c42a8265a9b09038d157ffbb7a
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-15 15:07:20 -0300

    📶 rest: add upload image files controller

--- src/equiny/rest/controllers/profiling/__init__.py
+++ src/equiny/rest/controllers/profiling/__init__.py
@@ -1,4 +1,9 @@
 from .create_horse_controller import CreateHorseController
 from .fetch_horse_controller import FetchHorseController
+from .upload_image_files_controller import UploadImageFilesController
 
-__all__ = ['CreateHorseController', 'FetchHorseController']
+__all__ = [
+    'CreateHorseController',
+    'FetchHorseController',
+    'UploadImageFilesController',
+]
