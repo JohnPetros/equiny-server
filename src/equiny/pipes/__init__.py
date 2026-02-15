43b92fe7d4e3ff9d572890534242e18c1a1c6a41
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-15 15:07:11 -0300

    📑 interface: add StoragePipe for file validation and provider injection

--- src/equiny/pipes/__init__.py
+++ src/equiny/pipes/__init__.py
@@ -1,5 +1,6 @@
 from .database_pipe import DatabasePipe
 from .pubsub_pipe import PubSubPipe
 from .providers_pipe import ProvidersPipe
+from .storage_pipe import StoragePipe
 
-__all__ = ['DatabasePipe', 'PubSubPipe', 'ProvidersPipe']
+__all__ = ['DatabasePipe', 'PubSubPipe', 'ProvidersPipe', 'StoragePipe']
--- src/equiny/pipes/providers_pipe.py
+++ src/equiny/pipes/providers_pipe.py
@@ -1,6 +1,8 @@
 from equiny.core.auth.interfaces.providers.hash_provider import HashProvider
+from equiny.core.storage.interfaces.file_storage_provider import FileStorageProvider
 from equiny.providers.hash import PwdlibHashProvider
 from equiny.providers.jwt import JoseJwtProvider
+from equiny.providers.storage.supabase import SupabaseFileStorageProvider
 
 
 class ProvidersPipe:
@@ -11,3 +13,7 @@ class ProvidersPipe:
     @staticmethod
     def get_jwt_provider() -> JoseJwtProvider:
         return JoseJwtProvider()
+
+    @staticmethod
+    def get_file_storage_provider() -> FileStorageProvider:
+        return SupabaseFileStorageProvider()
