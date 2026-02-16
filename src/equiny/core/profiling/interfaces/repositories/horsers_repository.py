7ef0084cdebaaf6956dcfc53f9c3ec881159be29
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-16 15:25:20 -0300

    📑 interface: extend repository and storage provider contracts

--- src/equiny/core/profiling/interfaces/repositories/horsers_repository.py
+++ src/equiny/core/profiling/interfaces/repositories/horsers_repository.py
@@ -1,5 +1,6 @@
 from typing import Protocol
 from equiny.core.profiling.domain.entities.horse import Horse
+from equiny.core.profiling.domain.structures.gallery import Gallery
 from equiny.core.profiling.domain.structures.image import Image
 from equiny.core.shared.domain.structures.id import Id
 
@@ -14,3 +15,9 @@ class HorsesRepository(Protocol):
     def find_by_id_and_owner_id(self, horse_id: Id, owner_id: Id) -> Horse | None: ...
 
     def add_many_images(self, horse_id: Id, images: list[Image]) -> None: ...
+
+    def find_gallery_by_horse_id(self, horse_id: Id) -> Gallery | None: ...
+
+    def replace(self, horse: Horse) -> None: ...
+
+    def delete_many_images(self, horse_id: Id) -> None: ...
