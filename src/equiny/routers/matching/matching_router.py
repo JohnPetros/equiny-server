87e2ca36a0d333623c65046d7a26acba03fbaf87
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-17 11:50:27 -0300

    🌐 domain: update matching router with feed endpoints

--- src/equiny/routers/matching/matching_router.py
+++ src/equiny/routers/matching/matching_router.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 
-from equiny.rest.controllers.matching import SwipeHorseController
+from equiny.rest.controllers.matching import (
+    DismatchHorseController,
+    ListMatchesController,
+    SwipeHorseController,
+)
 
 
 class MatchingRouter:
     @staticmethod
     def register() -> APIRouter:
         router = APIRouter(prefix='/matching', tags=['Matching module'])
+        DismatchHorseController.handle(router)
+        ListMatchesController.handle(router)
         SwipeHorseController.handle(router)
         return router
