5b15cca0340b49d0d369ab8af914ca0f16511d9f
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-01 11:07:24 -0300

    🌐 domain: add profiling domain with events and use cases with tests

--- src/equiny/core/profiling/domain/events/horse_match_notified_event.py
+++ src/equiny/core/profiling/domain/events/horse_match_notified_event.py
@@ -6,14 +6,17 @@ from equiny.core.profiling.domain.structures.dtos.horse_match_dto import HorseMa
 @dataclass
 class _Payload:
     horse_match: HorseMatchDto
+    owner_id: str
 
 
 class HorseMatchNotifiedEvent(Event[_Payload]):
     NAME: str = 'profiling/horse.match.notified'
 
-    def __init__(self, horse_match: HorseMatchDto) -> None:
+    def __init__(self, horse_match: HorseMatchDto, owner_id: str) -> None:
         payload = _Payload(
             horse_match=horse_match,
+            owner_id=owner_id,
         )
         super().__init__(HorseMatchNotifiedEvent.NAME, payload)
         self.horse_match = horse_match
+        self.owner_id = owner_id
