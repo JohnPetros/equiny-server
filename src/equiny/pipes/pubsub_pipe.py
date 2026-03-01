d357ae3b484c6b320f3df6edf5176b2d9e61dabd
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-01 11:07:35 -0300

    ⚙️ config: add pipes for providers and pubsub infrastructure

--- src/equiny/pipes/pubsub_pipe.py
+++ src/equiny/pipes/pubsub_pipe.py
@@ -2,7 +2,6 @@ from fastapi import Request, WebSocket
 
 from equiny.core.shared.interfaces.broker import Broker
 from equiny.pubsub.inngest.inngest_broker import InngestBroker
-from equiny.pubsub.redis.brokers.redis_matching_broker import RedisMatchingBroker
 
 
 class PubSubPipe:
@@ -14,8 +13,3 @@ class PubSubPipe:
     @staticmethod
     def get_redis_pubsub_from_websocket(websocket: WebSocket) -> Broker:
         return websocket.app.state.redis_pubsub
-
-    @staticmethod
-    def get_redis_matching_broker(request: Request) -> Broker:
-        redis_pubsub = request.app.state.redis_pubsub
-        return RedisMatchingBroker(redis_pubsub)
