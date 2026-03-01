a2c5361b5454561249b1607be646f6e9338e796b
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-01 20:16:36 -0300

    🌐 domain: update accounts faker with is_verified field

--- src/equiny/pipes/providers_pipe.py
+++ src/equiny/pipes/providers_pipe.py
@@ -1,8 +1,16 @@
+from equiny.core.auth.interfaces.providers.email_verification_provider import (
+    EmailVerificationProvider,
+)
 from equiny.core.auth.interfaces.providers.hash_provider import HashProvider
+from equiny.core.notification.interfaces.email_sender_provider import (
+    EmailProvider,
+)
 from equiny.core.shared.interfaces.cache_provider import CacheProvider
 from equiny.core.storage.interfaces.file_storage_provider import FileStorageProvider
+from equiny.providers.auth.itsdangerous import ItsdangerousEmailVerificationProvider
 from equiny.providers.hash import PwdlibHashProvider
 from equiny.providers.jwt import JoseJwtProvider
+from equiny.providers.email.resend import ResendEmailProvider
 from equiny.providers.storage.supabase import SupabaseFileStorageProvider
 from equiny.providers.cache.redis import RedisCacheProvider
 
@@ -23,3 +31,11 @@ class ProvidersPipe:
     @staticmethod
     def get_cache_provider() -> CacheProvider:
         return RedisCacheProvider()
+
+    @staticmethod
+    def get_email_verification_provider() -> EmailVerificationProvider:
+        return ItsdangerousEmailVerificationProvider()
+
+    @staticmethod
+    def get_email_provider() -> EmailProvider:
+        return ResendEmailProvider()
