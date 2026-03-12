dd107a43240aaf3e3409767075b1a7be4f19cb3a
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-12 13:21:45 -0300

    ♻️ refactor: change owner parameter type to Id in UpdateHorseController

--- src/equiny/rest/controllers/profiling/update_horse_controller.py
+++ src/equiny/rest/controllers/profiling/update_horse_controller.py
@@ -2,9 +2,9 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends
 
 from equiny.core.profiling.domain.entities.dtos import HorseDto
-from equiny.core.profiling.domain.entities.owner import Owner
 from equiny.core.profiling.interfaces.repositories import HorsesRepository
 from equiny.core.profiling.use_cases import UpdateHorseUseCase
+from equiny.core.shared.domain.structures.id import Id
 from equiny.pipes.database_pipe import DatabasePipe
 from equiny.pipes.profiling_pipe import ProfilingPipe
 from equiny.validation.profiling import HorseSchema
@@ -21,8 +21,8 @@ class UpdateHorseController:
         def _(
             horse_id: str,
             body: HorseSchema,
-            owner: Owner = Depends(ProfilingPipe.get_owner_id),
+            owner_id: Id = Depends(ProfilingPipe.get_owner_id),
             repository: HorsesRepository = Depends(DatabasePipe.get_horses_repository),
         ) -> HorseDto:
             use_case = UpdateHorseUseCase(repository)
-            return use_case.execute(horse_id, owner.id.value, body.to_dto())
+            return use_case.execute(horse_id, owner_id.value, body.to_dto())
